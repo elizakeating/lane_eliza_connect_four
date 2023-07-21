197da5bb40a14cff7b84a0256a8fcadbdaa9fbd6
Lane Bretschneider <lanebret85@gmail.com>
2023-07-21 15:05:45 -0700

    refactor: modify tests for readable attributes and add_spaces methods

--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -13,25 +13,40 @@ RSpec.describe GameBoard do
     it "has readable attributes" do
       game_board = GameBoard.new
 
-      expect(game_board.board).to eq([])
+      expect(game_board.board).to eq([
+        ["A"],
+        ["B"],
+        ["C"],
+        ["D"],
+        ["E"],
+        ["F"],
+        ["G"]
+      ])
       expect(game_board.space).to eq(".")
-      expect(game_board.column).to eq([])
+
+      expect(game_board.column_1).to eq(["A"])
+      expect(game_board.column_2).to eq(["B"])
+      expect(game_board.column_3).to eq(["C"])
+      expect(game_board.column_4).to eq(["D"])
+      expect(game_board.column_5).to eq(["E"])
+      expect(game_board.column_6).to eq(["F"])
+      expect(game_board.column_7).to eq(["G"])
     end
   end
 
   describe "#add_spaces" do
     it "can add spaces to the baord" do
       game_board = GameBoard.new
-      game_board.add_speaces
+      game_board.add_spaces
 
       expect(game_board.board).to eq([
-      ["A", ".", ".", ".", ".", ".", "."],
-      ["B", ".", ".", ".", ".", ".", "."],
-      ["C", ".", ".", ".", ".", ".", "."],
-      ["D", ".", ".", ".", ".", ".", "."],
-      ["E", ".", ".", ".", ".", ".", "."],
-      ["F", ".", ".", ".", ".", ".", "."],
-      ["G", ".", ".", ".", ".", ".", "."]
+        ["A", ".", ".", ".", ".", ".", "."],
+        ["B", ".", ".", ".", ".", ".", "."],
+        ["C", ".", ".", ".", ".", ".", "."],
+        ["D", ".", ".", ".", ".", ".", "."],
+        ["E", ".", ".", ".", ".", ".", "."],
+        ["F", ".", ".", ".", ".", ".", "."],
+        ["G", ".", ".", ".", ".", ".", "."]
       ])
     end
   end
