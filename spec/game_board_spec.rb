3eb55747d80a4d251d9b693e971bd7e1c4cab040
Lane Bretschneider <lanebret85@gmail.com>
2023-07-21 14:19:34 -0700

    test: add tests for GameBoard class

---
+++ spec/game_board_spec.rb
@@ -0,0 +1,38 @@
+require "./lib/game_board"
+
+RSpec.describe GameBoard do
+  describe "#initializes" do
+    it "can initialize" do
+      game_board = GameBoard.new
+
+      expect(game_board).to be_a(GameBoard)
+    end
+  end
+
+  describe "#readable attributes" do
+    it "has readable attributes" do
+      game_board = GameBoard.new
+
+      expect(game_board.board).to eq([])
+      expect(game_board.space).to eq(".")
+      expect(game_board.columns).to eq([])
+    end
+  end
+
+  describe "#add_spaces" do
+    it "can add spaces to the baord" do
+      game_board = GameBoard.new
+      game_board.add_speaces
+
+      expect(game_board.board).to eq([
+      ["A", ".", ".", ".", ".", ".", "."],
+      ["B", ".", ".", ".", ".", ".", "."],
+      ["C", ".", ".", ".", ".", ".", "."],
+      ["D", ".", ".", ".", ".", ".", "."],
+      ["E", ".", ".", ".", ".", ".", "."],
+      ["F", ".", ".", ".", ".", ".", "."],
+      ["G", ".", ".", ".", ".", ".", "."]
+      ])
+    end
+  end
+end
