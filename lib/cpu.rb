30e517484cb35c9a91d5efad1e174e28365b3869
Lane Bretschneider <lanebret85@gmail.com>
2023-07-22 15:06:34 -0700

    feat: add Cpu class with initialize and random_letter methods

---
+++ lib/cpu.rb
@@ -0,0 +1,13 @@
+class Cpu
+  attr_reader :cpu_token
+  
+  def initialize(cpu_token)
+    @cpu_token = cpu_token
+  end
+
+  def random_letter
+    letters = ["A", "B", "C", "D", "E", "F", "G"]
+
+    letters.sample
+  end
+end
