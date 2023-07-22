59a352f36cbab2b3113cdffe617486def071ad4c
Lane Bretschneider <lanebret85@gmail.com>
2023-07-21 23:03:38 -0700

    refactor: modify tests for Player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -19,12 +19,14 @@ RSpec.describe Player do
 
   describe "#choose_column" do
     it "can choose a column from the game board" do
+      game_board = GameBoard.new
+      game_board.add_spaces
       player = Player.new("X")
       cpu = Cpu.new("O")
 
       player.choose_column("A")
 
-      expect(game_board.column_1[1]).to eq("X")
+      expect(game_board.column_1[6]).to eq("X")
 
       cpu.choose_column("A")
 
