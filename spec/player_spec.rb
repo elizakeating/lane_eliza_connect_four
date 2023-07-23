ef519e411a45a592a4df7484d6b0bbcb429b1ff2
Eliza Keating <ek.elizakeating@gmail.com>
2023-07-22 23:40:19 -0600

    test: add possible game board attribute tests for player

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,28 +5,45 @@ require "./lib/game_board"
 RSpec.describe Player do
   describe "#initialize" do
     it "exists" do
-      player = Player.new("X")
+      game_board = GameBoard.new
+      game_board.add_spaces
+
+      player = Player.new("X", game_board)
       
       expect(player).to be_a(Player)
     end
+  end
     
     describe "#piece_type" do
       it "returns piece type of player" do
-        player = Player.new("X")
+        game_board = GameBoard.new
+        game_board.add_spaces
+
+        player = Player.new("X", game_board)
 
         expect(player.piece_type).to eq("X")
       end
     end
-  end
+
+    describe "#board" do
+      it "returns board" do
+        game_board = GameBoard.new
+        game_board.add_spaces
+
+        player = Player.new("X", game_board)
+
+        expect(player.board).to be_a(GameBoard)
+      end
+    end
   
   describe "#choose_column" do
     it "allows player to choose a column to place their piece" do
       game_board = GameBoard.new
       game_board.add_spaces
-      
-      player = Player.new("X")
 
-      player.choose_column("D")
+      player = Player.new("X", game_board)
+
+      player.choose_column("A")
 
       expect(game_board.board).to eq(
         [
