01b95d9d9030da03c45ddded801103fb35a2731f
Eliza Keating <ek.elizakeating@gmail.com>
2023-07-23 11:58:09 -0600

    test: add cpu tests for turn module

--- spec/cpu_spec.rb
+++ spec/cpu_spec.rb
@@ -57,4 +57,124 @@ RSpec.describe CPU do
       )
     end
   end
+
+  describe "#is_full?" do
+    it "returns false when the board is not full" do
+      game_board = GameBoard.new
+      game_board.add_spaces
+      
+      cpu = CPU.new("O", game_board)
+      
+      expect(cpu.is_full?).to eq(false)
+      
+      cpu.choose_column("D")
+      
+      expect(cpu.is_full?).to eq(false)
+      
+      cpu.choose_column("D")
+      cpu.choose_column("B")
+      cpu.choose_column("C")
+      cpu.choose_column("E")
+      
+      expect(cpu.is_full?).to eq(false)
+    end
+    
+    it "returns true when the board is full" do
+      game_board = GameBoard.new
+      game_board.add_spaces
+      
+      cpu = CPU.new("O", game_board)
+      
+      expect(cpu.is_full?).to eq(false)
+      
+      6.times do
+        cpu.choose_column("A")
+      end
+      6.times do
+        cpu.choose_column("B")
+      end
+      6.times do
+        cpu.choose_column("C")
+      end
+      6.times do
+        cpu.choose_column("D")
+      end
+      6.times do
+        cpu.choose_column("E")
+      end
+      6.times do
+        cpu.choose_column("F")
+      end
+      6.times do
+        cpu.choose_column("G")
+      end
+      
+      expect(cpu.is_full?).to eq(true)
+    end
+  end
+  
+  describe "#win?" do
+    it "returns false by default" do
+      game_board = GameBoard.new
+      game_board.add_spaces
+      
+      cpu = CPU.new("O", game_board)
+      
+      cpu.choose_column("D")
+      cpu.choose_column("B")
+      cpu.choose_column("C")
+      cpu.choose_column("E")
+      
+      expect(cpu.win?).to eq(false)
+    end
+    
+    it "returns true for horizontal win" do
+      game_board = GameBoard.new
+      game_board.add_spaces
+      
+      cpu = CPU.new("O", game_board)
+      
+      cpu.choose_column("A")
+      cpu.choose_column("B")
+      cpu.choose_column("C")
+      cpu.choose_column("D")
+      
+      expect(cpu.win?).to eq(true)
+    end
+    
+    it "returns true for vertical win" do
+      game_board = GameBoard.new
+      game_board.add_spaces
+      
+      cpu = CPU.new("O", game_board)
+      
+      cpu.choose_column("D")
+      cpu.choose_column("D")
+      cpu.choose_column("D")
+      cpu.choose_column("D")
+      
+      expect(cpu.win?).to eq(true)
+    end
+    
+    it "returns true for diagonal win" do
+      game_board = GameBoard.new
+      game_board.add_spaces
+      
+      player = Player.new("X", game_board)
+      cpu = CPU.new("O", game_board)
+      
+      cpu.choose_column("C")
+      cpu.choose_column("D")
+      cpu.choose_column("D")
+      cpu.choose_column("E")
+      cpu.choose_column("E")
+      cpu.choose_column("E")
+      player.choose_column("F")
+      cpu.choose_column("F")
+      cpu.choose_column("F")
+      cpu.choose_column("F")
+
+      expect(cpu.win?).to eq(true)
+    end
+  end
 end
