6cbf49cde836a7630ba2d7ed512822b633150e9c
Lane Bretschneider <lanebret85@gmail.com>
2023-07-24 11:24:30 -0700

    add vertical_win?, start horizontal_win?

--- lib/turnable.rb
+++ lib/turnable.rb
@@ -4,13 +4,78 @@ module Turnable
   end
 
   def find_spaces
-    board.board.find {
+    board.board.find do
       |column| column.include? "."
-    }
+    end
   end
 
-  def win?
-    
+  def vertical_win?
+    win = false
+    board.board.each do |column|
+      win = true if column[1..4] == ["X", "X", "X", "X"] ||
+      column[2..5] == ["X", "X", "X", "X"] ||
+      column[3..6] == ["X", "X", "X", "X"] ||
+      column[1..4] == ["O", "O", "O", "O"] ||
+      column[2..5] == ["O", "O", "O", "O"] ||
+      column[3..6] == ["O", "O", "O", "O"]
+    end
+    win
+  end
+
+  def horizontal_win?
+  require 'pry';binding.pry
+    (board.board[0][1] == "X" &&
+    board.board[1][1] == "X" &&
+    board.board[2][1] == "X" &&
+    board.board[3][1] == "X") ||
+    board.board[1..4][1] == ["X", "X", "X", "X"] ||
+    board.board[2..5][1] == ["X", "X", "X", "X"] ||
+    board.board[3..6][1] == ["X", "X", "X", "X"] ||
+    board.board[0..3][1] == ["O", "O", "O", "O"] ||
+    board.board[1..4][1] == ["O", "O", "O", "O"] ||
+    board.board[2..5][1] == ["O", "O", "O", "O"] ||
+    board.board[3..6][1] == ["O", "O", "O", "O"] ||
+    board.board[0..3][2] == ["X", "X", "X", "X"] ||
+    board.board[1..4][2] == ["X", "X", "X", "X"] ||
+    board.board[2..5][2] == ["X", "X", "X", "X"] ||
+    board.board[3..6][2] == ["X", "X", "X", "X"] ||
+    board.board[0..3][2] == ["O", "O", "O", "O"] ||
+    board.board[1..4][2] == ["O", "O", "O", "O"] ||
+    board.board[2..5][2] == ["O", "O", "O", "O"] ||
+    board.board[3..6][2] == ["O", "O", "O", "O"] ||
+    board.board[0..3][3] == ["X", "X", "X", "X"] ||
+    board.board[1..4][3] == ["X", "X", "X", "X"] ||
+    board.board[2..5][3] == ["X", "X", "X", "X"] ||
+    board.board[3..6][3] == ["X", "X", "X", "X"] ||
+    board.board[0..3][3] == ["O", "O", "O", "O"] ||
+    board.board[1..4][3] == ["O", "O", "O", "O"] ||
+    board.board[2..5][3] == ["O", "O", "O", "O"] ||
+    board.board[3..6][3] == ["O", "O", "O", "O"] ||
+    board.board[0..3][4] == ["X", "X", "X", "X"] ||
+    board.board[1..4][4] == ["X", "X", "X", "X"] ||
+    board.board[2..5][4] == ["X", "X", "X", "X"] ||
+    board.board[3..6][4] == ["X", "X", "X", "X"] ||
+    board.board[0..3][4] == ["O", "O", "O", "O"] ||
+    board.board[1..4][4] == ["O", "O", "O", "O"] ||
+    board.board[2..5][4] == ["O", "O", "O", "O"] ||
+    board.board[3..6][4] == ["O", "O", "O", "O"] ||
+    board.board[0..3][5] == ["X", "X", "X", "X"] ||
+    board.board[1..4][5] == ["X", "X", "X", "X"] ||
+    board.board[2..5][5] == ["X", "X", "X", "X"] ||
+    board.board[3..6][5] == ["X", "X", "X", "X"] ||
+    board.board[0..3][5] == ["O", "O", "O", "O"] ||
+    board.board[1..4][5] == ["O", "O", "O", "O"] ||
+    board.board[2..5][5] == ["O", "O", "O", "O"] ||
+    board.board[3..6][5] == ["O", "O", "O", "O"] ||
+    board.board[0..3][6] == ["X", "X", "X", "X"] ||
+    board.board[1..4][6] == ["X", "X", "X", "X"] ||
+    board.board[2..5][6] == ["X", "X", "X", "X"] ||
+    board.board[3..6][6] == ["X", "X", "X", "X"] ||
+    board.board[0..3][6] == ["O", "O", "O", "O"] ||
+    board.board[1..4][6] == ["O", "O", "O", "O"] ||
+    board.board[2..5][6] == ["O", "O", "O", "O"] ||
+    board.board[3..6][6] == ["O", "O", "O", "O"]
+    win
   end
 
   def diagonal_win?
